879316ee818b8ca9f2e0d95013a05fd5e2edde25
Breno Calazans <brenocalazans@gmail.com>
2016-12-18 16:38:55 -0200

    Make functional the hell out of it

--- day-6/part-1.js
+++ day-6/part-1.js
@@ -1,31 +1,8 @@
-module.exports.solvePart1 = function solve(input) {
-  const parsedInput = parseInput(input)
-
-  const message = parsedInput.reduce((message, line) => message + mostFrequent(line), '')
-
-  return message
-}
-
-module.exports.solvePart2 = function solve(input) {
-  const parsedInput = parseInput(input)
-
-  const message = parsedInput.reduce((message, line) => message + leastFrequent(line), '')
-
-  return message
-}
-
-const leastFrequent = (line) => {
-  const count = line.reduce(incrementOrInsert, {})
-  const countArray = objToArray(count).sort(sort)
-  return countArray[countArray.length - 1].char
-}
-
-const mostFrequent = (line) => {
-  const count = line.reduce(incrementOrInsert, {})
-  const countArray = objToArray(count).sort(sort)
-  return countArray[0].char
-}
-
+const takeFirst = (array) => array[0]
+const takeLast = (array) => array[array.length - 1]
+const countLine = (line) => line.reduce(incrementOrInsert, {})
+const sortCount = (count) => objToArray(count).sort(sort)
+const getChar = (obj) => obj.char
 const sort = (a, b) => a.value > b.value ? -1 : 1
 
 const objToArray = (obj) => {
@@ -46,11 +23,20 @@ const incrementOrInsert = (count, char) => {
 const parseInputStringToArray = (input) => input.split('\n').map(line => line.split(''))
 
 const transposeMatrix = (array) =>
-  array[0].map((col, i) =>
-    array.map(row => row[i])
-  )
+  array[0].map((col, i) => array.map(row => row[i]))
 
 const compose = (...funcs) =>
   funcs.reduce((memo, b) => (...args) => memo(b(...args)))
 
 const parseInput = compose(transposeMatrix, parseInputStringToArray)
+const leastFrequent = compose(getChar, takeLast, sortCount, countLine)
+const mostFrequent = compose(getChar, takeFirst, sortCount, countLine)
+
+const getMessage = (fn) =>
+  (input) =>
+    input.reduce((message, line) =>
+      message + fn(line)
+    , '')
+
+module.exports.solvePart1 = compose(getMessage(mostFrequent), parseInput)
+module.exports.solvePart2 = compose(getMessage(leastFrequent), parseInput)
